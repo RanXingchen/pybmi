d98de46ca1d114c32d90114123fd6f77b9885ece
Ran Xingchen <rexran@sina.com>
2021-08-22 15:32:40 +0800

    import pybmi!

--- __init__.py
+++ __init__.py
@@ -1,6 +1,19 @@
-import pybmi.data
-import pybmi.fitting
-import pybmi.signal
-import pybmi.special
-import pybmi.stats
-import pybmi.utils      # noqa
+from pybmi import data
+from pybmi import fitting
+from pybmi import signal
+from pybmi import special
+from pybmi import stats
+from pybmi import utils
+from pybmi import optimization
+
+
+
+__all__ = [
+    'data',
+    'fitting',
+    'signal',
+    'special',
+    'stats',
+    'utils',
+    'optimization'
+]
