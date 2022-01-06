29e70849117b4433ae979c517b458676b7d0d6c4
Ran Xingchen <rexran@sina.com>
2022-01-06 18:14:58 +0800

    fix bugs flake8 not working.

--- __init__.py
+++ __init__.py
@@ -1,17 +1,16 @@
 from pybmi import data
 from pybmi import fitting
-from pybmi import signal
+from pybmi import signals
 from pybmi import special
 from pybmi import stats
 from pybmi import utils
 from pybmi import optimization
 
 
-
 __all__ = [
     'data',
     'fitting',
-    'signal',
+    'signals',
     'special',
     'stats',
     'utils',
