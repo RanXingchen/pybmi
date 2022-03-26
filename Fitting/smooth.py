0e70dd3ba9ee949e3fb52939a53d9d0a918c672b
Ran Xingchen <rexran@sina.com>
2022-03-26 17:30:18 +0800

    update for smooth: new for PA

--- Fitting/smooth.py
+++ Fitting/smooth.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from pybmi.utils.utils import check_params
+from ..utils.utils import check_params
 
 
 class Smooth():
@@ -16,11 +16,12 @@ class Smooth():
     method : str, optional
         Smooths data Y with specified METHOD. The available methods
         are:
-            'moving' - Moving average (default)
+            'MA' - Moving average (default)
+            'PA' - Past average.
 
     Notes
     -----
-    1. For the 'moving' methods, SPAN must be odd. If an even SPAN
+    1. For the 'MA' methods, SPAN must be odd. If an even SPAN
        is specified, it is reduced by 1.
     2. If SPAN is greater than the length of Y, it is reduced to
        the length of Y.
@@ -28,12 +29,17 @@ class Smooth():
     Examples
     --------
     >>> y = np.random.randn((1000, 5))
-    >>> smoother = Smooth(span=5, method='moving')
+    >>> smoother = Smooth(span=5, method='MA')
     >>> smoothed_y = smoother.apply(y)
     """
-    def __init__(self, span=5, method='moving'):
+    def __init__(self, span=5, method='MA'):
+        methods = ['MA', 'PA']
+        self.method = check_params(method, methods, 'method')
+
         self.span = span
-        self.method = check_params(method, ['moving'], 'method')
+        # Make sure for moving average, the span is odd.
+        if self.method == 'MA' and span % 2 == 0:
+            self.span -= 1
 
     def apply(self, y):
         """
@@ -59,7 +65,7 @@ class Smooth():
             istensor = False
 
         smoothed = y.copy()
-        if self.method == 'moving':
+        if self.method == 'MA':
             for n, i in enumerate(y.T):
                 ii = np.convolve(
                     i, np.ones((self.span,)) / self.span, mode='valid'
@@ -68,6 +74,11 @@ class Smooth():
                 beg = np.cumsum(i[:self.span - 1])[::2] / r
                 end = (np.cumsum(i[:-self.span:-1])[::2] / r)[::-1]
                 smoothed[:, n] = np.concatenate((beg, ii, end))
+        elif self.method == 'PA':
+            n = self.span - 1
+            for i in range(n - 1, -1, -1):
+                smoothed[n:] += y[i:i - n]
+            smoothed[n:] /= self.span
 
         if istensor:
             smoothed = torch.tensor(smoothed, dtype=dtype).to(device)
