8e7fddcf2ded3c47c31f2709314b72140a93126d
Ran Xingchen <rexran@sina.com>
2022-05-04 14:27:02 +0800

    update for feature selection

--- stats/feat_selection.py
+++ stats/feat_selection.py
@@ -1,50 +1,84 @@
 import numpy as np
-import pybmi as bmi
+import warnings
 
 from scipy.stats import ttest_ind
 
+from .statistics import wanova1
+from ..utils import check_params
+
 
 def feature_selection(features: np.ndarray, labels: np.ndarray,
-                      p: float = 0.01, method: str = None):
+                      threshold: float = 0.01, method: str = 'welch',
+                      keep_dims=False):
     """
-    Selecting the features with significant difference with different label.
+    Selecting the features with significant difference with different labels.
 
     Parameters
     ----------
     features : ndarray
         The object features which have shape [L, N] wanted to do feature
-        selection. In which L means the number of samples, and N means the
-        number of features.
+        selection. In which L means the number of samples, and N means
+        the number of features.
     labels : ndarray
-        The labels indicate different state for the features. Labels should
-        have be a one dimensional vector with length of L.
+        The labels indicate different state for the features. Labels must
+        be a one dimensional vector with length of L.
     p : float, optional
         The significant level.
     method : str, optional
-        The statistic method used to compute the significance. The option
-        can be one of: 'welch',
-        Default is None, means that if the labels only have 2 state, using
-        '' as the method; otherwise, use welch's ANOVA as the method.
+        The statistic method used to compute the significance. The current
+        option only 'welch'.
+        Default is 'welch', if the labels only have 2 state, using
+        welch's t-test, otherwise, use welch's ANOVA.
+    keep_dims : bool, optional
+        Indicate whether keep the original number of dimensions. If True,
+        the features with no testing significance will be replaced to zeros.
+        Otherwise, these features are no longer exist in the return results.
+        Default: False.
     """
+    method = check_params(method, ['welch'], 'method')
+
+    assert features.ndim == 2, \
+        'Do not accept features with number of dimensions greater than 2.'
+    assert labels.ndim == 1, "Do not accept multi-dimensional labels."
+
+    T = features.shape[-2]  # The length of the features.
+    N = features.shape[-1]  # The number of the features.
+
+    assert T == len(labels), \
+        "The length of features must equal to the length of states."
+
     states = np.unique(labels)
 
-    # Initialize the p values.
-    sigs = np.zeros((features.shape[-1],))
     # Check the number of states, and do statistics test.
     if len(states) < 2:
+        warnings.warn(
+            message=f'The state of the given LABEL only contain {len(states)} '
+                    'category. No feature selection applied, the original '
+                    'features have been returned.',
+            category=type[UserWarning]
+        )
         return features
     elif len(states) == 2:
+        # Two sample test.
         x0 = features[labels == states[0]]
         x1 = features[labels == states[1]]
-        if method is None or method == 'welch':
-            _, sigs = ttest_ind(x0, x1, equal_var=False, axis=0)
+        if method == 'welch':
+            _, p = ttest_ind(x0, x1, equal_var=False, axis=0)
         # TODO: Add more methods.
     else:
-        if method is None or method == 'welch':
+        # Initialize the p values.
+        p = np.zeros((N,))
+
+        if method == 'welch':
             for i, feature in enumerate(features.T):
-                sigs[i] = bmi.stats.wanova1(feature, labels)
+                p[i] = wanova1(feature, labels)
         # TODO: Add more methods.
 
     # Selecting the features which had p value smaller than threshold.
-    indices = sigs < p
-    return features[:, indices], sigs
+    indices = p < threshold
+
+    if keep_dims:
+        features[:, ~indices] = 0
+    else:
+        features = features[:, indices]
+    return features, p
